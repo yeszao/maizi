cb03987d68b8090559ca94f559cb91aea807cabc
yeszao <yeszao@qq.com>
2017-09-11 16:34:09 +0800

    slider width and height set to 100%

--- includes/widget-slider-unit.php
+++ includes/widget-slider-unit.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 add_action( 'widgets_init', 'qiaomi_widgets' );
 
 function qiaomi_widgets() {
@@ -8,6 +8,7 @@ function qiaomi_widgets() {
 function wp_media_script() {
 	wp_enqueue_media();
 }
+
 add_action( 'admin_enqueue_scripts', 'wp_media_script' );
 
 
@@ -18,8 +19,10 @@ function widgets_scripts( $hook ) {
 	wp_enqueue_style( 'wp-color-picker' );
 	wp_enqueue_script( 'wp-color-picker' );
 	wp_enqueue_script( 'qiaomi-admin-js', get_template_directory_uri() . '/assets/js/qiaomi-admin.js', array(
-		'jquery' ), false, true );
+		'jquery'
+	), false, true );
 }
+
 add_action( 'admin_enqueue_scripts', 'widgets_scripts' );
 
 
@@ -29,147 +32,147 @@ add_action( 'admin_enqueue_scripts', 'widgets_scripts' );
  */
 class Slider_Unit_Widget extends WP_Widget {
 	private $defaults = array(
-		'title' => '',
+		'title'    => '',
 		'bg_color' => '#ffffff',
-		'img' => '',
+		'img'      => '',
 		'img_link' => '',
-		'caption' => '',
-		'height' => '350px',
-		'is_full' => '',
+		'caption'  => '',
+		'height'   => '350px',
+		'is_full'  => '',
 	);
 
-	function __construct()
-	{
-		parent::__construct(false, 'Slider Unit', array('description' => 'Add this unit to index slider'));
+	function __construct() {
+		parent::__construct( false, 'Slider Unit', array( 'description' => 'Add this unit to index slider' ) );
 	}
 
 	function widget( $args, $instance ) {
 		extract( $args );
-        extract($instance);
+		extract( $instance );
 
-		apply_filters('widget_name', $instance['title']);
+		apply_filters( 'widget_name', $instance['title'] );
 
-        $text = $before_widget;
-        $text .= "<div class='w-100' style='background-color:$bg_color;height:$height'>";
+		$text = $before_widget;
+		$text .= "<div class='w-100' style='background-color:$bg_color;max-height:$height'>";
 
-        if (isset($is_full) && $is_full) {
-            $text .= '<div class="w-100 h-100">';
-        } else {
-            $text .= '<div class="container h-100">';
-        }
+		if ( isset( $is_full ) && $is_full ) {
+			$text .= '<div class="w-100 h-100">';
+		} else {
+			$text .= '<div class="container h-100">';
+		}
 
-        $text .= $img_link ? "<a href='$img_link' class='w-100 h-100'>" : '';
+		$text .= $img_link ? "<a href='$img_link' class='w-100 h-100'>" : '';
 
-        $img_url = wp_get_attachment_image_src($img, 'full')[0];
-        $text .= $img ? "<img class='d-block w-100 img-fluid' src='$img_url' />" : '';
+		$img_url = wp_get_attachment_image_src( $img, 'full' )[0];
+		$text    .= $img ? "<img class='d-block w-100 h-100 img-fluid' src='$img_url' />" : '';
 
-        $text .= $img_link ? '</a>' : '';
+		$text .= $img_link ? '</a>' : '';
 
-        if ($caption) {
-            $text .= '<div class="carousel-caption  d-md-block">';
-            $text .= $caption;
-            $text .= '</div>';
-        }
+		if ( $caption ) {
+			$text .= '<div class="carousel-caption  d-md-block">';
+			$text .= $caption;
+			$text .= '</div>';
+		}
 
-        $text .= '</div></div>';
+		$text .= '</div></div>';
 
-        $text .= $after_widget;
+		$text .= $after_widget;
 
-        echo $text;
+		echo $text;
 	}
 
-	function form($instance) {
-		$instance = wp_parse_args((array)$instance, $this->defaults);
-?>
-		<p>
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, $this->defaults );
+		?>
+        <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">
-                <?php _e( 'Title' ); ?>
+				<?php _e( 'Title' ); ?>
             </label>
             <input class="widefat" type="text"
                    id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
                    name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>"
-                   value="<?php echo esc_attr( $instance['title'] ); ?>" />
-		</p>
+                   value="<?php echo esc_attr( $instance['title'] ); ?>"/>
+        </p>
 
-		<p>
+        <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'bg_color' ) ); ?>">
-                <?php _e('Background color' ); ?>
+				<?php _e( 'Background color' ); ?>
             </label>
-            <br />
+            <br/>
             <input class="color-picker" type="text"
                    id="<?php echo esc_attr( $this->get_field_id( 'bg_color' ) ); ?>"
-                   name="<?php echo $this->get_field_name('bg_color'); ?>"
-                   value="<?php echo $instance['bg_color']; ?>" />
-		</p>
+                   name="<?php echo $this->get_field_name( 'bg_color' ); ?>"
+                   value="<?php echo $instance['bg_color']; ?>"/>
+        </p>
 
         <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'height' ) ); ?>">
-                <?php _e('Height' ); ?>
+				<?php _e( 'Height' ); ?>
             </label>
             <input class="widefat" type="text"
                    id="<?php echo esc_attr( $this->get_field_id( 'height' ) ); ?>"
                    name="<?php echo esc_attr( $this->get_field_name( 'height' ) ); ?>"
-                   value="<?php echo esc_attr( $instance['height'] ); ?>" />
+                   value="<?php echo esc_attr( $instance['height'] ); ?>"/>
         </p>
 
-		<div class="upload-img-box">
+        <div class="upload-img-box">
             <label for="<?php echo esc_attr( $this->get_field_id( 'img' ) ); ?>">
-                <?php _e('Image' ); ?>
+				<?php _e( 'Image' ); ?>
             </label>
-			<div class="media-widget-control">
-				<div class="media-widget-preview">
-					<div class="attachment-media-view custom-img-container">
-						<?php if ($instance['img']) { ?>
-							<img src="<?php echo wp_get_attachment_image_src( $instance['img'], 'full' )[0]; ?>"
-								 style="max-width: 100%;" />
+            <div class="media-widget-control">
+                <div class="media-widget-preview">
+                    <div class="attachment-media-view custom-img-container">
+						<?php if ( $instance['img'] ) { ?>
+                            <img src="<?php echo wp_get_attachment_image_src( $instance['img'], 'full' )[0]; ?>"
+                                 style="max-width: 100%;"/>
 						<?php } else { ?>
-							<div class="placeholder">
-								<?php _e('No image selected'); ?>
-							</div>
+                            <div class="placeholder">
+								<?php _e( 'No image selected' ); ?>
+                            </div>
 						<?php } ?>
-					</div>
-				</div>
-			</div>
-			<input class="widefat custom-img-id" type="hidden"
+                    </div>
+                </div>
+            </div>
+            <input class="widefat custom-img-id" type="hidden"
                    id="<?php echo esc_attr( $this->get_field_id( 'img' ) ); ?>"
-                   name="<?php echo $this->get_field_name('img'); ?>"
-                   value="<?php echo $instance['img']; ?>" />
-			<a class="button upload-custom-img"><?php _e( 'Select Image' ); ?></a>
-			<a class="button delete-custom-img<?php echo $instance['img'] ?'':' hidden'; ?>">
-                <?php _e( 'Remove Image'); ?>
+                   name="<?php echo $this->get_field_name( 'img' ); ?>"
+                   value="<?php echo $instance['img']; ?>"/>
+            <a class="button upload-custom-img"><?php _e( 'Select Image' ); ?></a>
+            <a class="button delete-custom-img<?php echo $instance['img'] ? '' : ' hidden'; ?>">
+				<?php _e( 'Remove Image' ); ?>
             </a>
-		</div>
-
-		<p>
-                <label for="<?php echo esc_attr( $this->get_field_id( 'caption' ) ); ?>">
-                    <?php _e( 'Caption' ); ?>
-                </label>
-				<textarea class="widefat"
-                          id="<?php echo esc_attr( $this->get_field_id( 'caption' ) ); ?>"
-                          name="<?php echo $this->get_field_name('caption'); ?>" rows="6" ><?php echo esc_textarea($instance['caption']); ?></textarea>
-		</p>
-        
-		<p>
-            <label for="<?php echo esc_attr( $this->get_field_id( 'img_link' ) ); ?>">
-                <?php _e( 'Links' ); ?>
-            </label>
-            <input  class="widefat" type="text"
-                    id="<?php echo esc_attr( $this->get_field_id( 'img_link' ) ); ?>"
-                    name="<?php echo $this->get_field_name('img_link'); ?>"
-                    value="<?php echo $instance['img_link']; ?>" />
-		</p>
+        </div>
 
         <p>
-            <input  class="widefat" type="checkbox"
-                    id="<?php echo esc_attr( $this->get_field_id( 'is_full' ) ); ?>"
-                    name="<?php echo $this->get_field_name('is_full'); ?>"
-                    value="1"
-                    <?php echo $instance['is_full'] ? 'checked' : ''; ?> />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'caption' ) ); ?>">
+				<?php _e( 'Caption' ); ?>
+            </label>
+            <textarea class="widefat"
+                      id="<?php echo esc_attr( $this->get_field_id( 'caption' ) ); ?>"
+                      name="<?php echo $this->get_field_name( 'caption' ); ?>"
+                      rows="6"><?php echo esc_textarea( $instance['caption'] ); ?></textarea>
+        </p>
+
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'img_link' ) ); ?>">
+				<?php _e( 'Links' ); ?>
+            </label>
+            <input class="widefat" type="text"
+                   id="<?php echo esc_attr( $this->get_field_id( 'img_link' ) ); ?>"
+                   name="<?php echo $this->get_field_name( 'img_link' ); ?>"
+                   value="<?php echo $instance['img_link']; ?>"/>
+        </p>
+
+        <p>
+            <input class="widefat" type="checkbox"
+                   id="<?php echo esc_attr( $this->get_field_id( 'is_full' ) ); ?>"
+                   name="<?php echo $this->get_field_name( 'is_full' ); ?>"
+                   value="1"
+				<?php echo $instance['is_full'] ? 'checked' : ''; ?> />
             <label for="<?php echo esc_attr( $this->get_field_id( 'is_full' ) ); ?>">
-                <?php _e( 'Full width' ); ?>
+				<?php _e( 'Full width' ); ?>
             </label>
         </p>
-<?php
+		<?php
 	}
 }
 
--- sidebar-slider.php
+++ sidebar-slider.php
@@ -3,31 +3,30 @@
  * Index slider
  *
  */
+
+$count = count_widgets( 'index-slider' ) ?: 0;
 ?>
 
-<?php if ( is_active_sidebar( 'index-slider' ) ) : ?>
+<?php if ( is_active_sidebar( 'index-slider' ) && $count > 0 ) : ?>
 
 	<div class="wrapper py-0">
 
 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 
-            <?php $count = count_widgets('index-slider') ?: 0; ?>
+			<?php if ( $count > 1 ) : ?>
 
-            <?php if ($count > 1): ?>
+				<ol class="carousel-indicators">
 
-                <ol class="carousel-indicators">
+					<?php for ( $i = 0; $i < $count; $i ++ ) : ?>
 
-                    <?php for($i = 0; $i < $count; $i++) : ?>
+						<li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $i ?>"
+							<?php if ( $i === 0 ): ?>class="active" <?php endif; ?>></li>
 
-                        <li data-target="#carouselExampleIndicators"
-                            data-slide-to="<?php echo $i ?>"
-                            <?php if ($i === 0): ?>class="active" <?php endif; ?>></li>
+					<?php endfor; ?>
 
-                    <?php endfor; ?>
+				</ol>
 
-                </ol>
-
-            <?php endif; ?>
+			<?php endif; ?>
 
 			<div class="carousel-inner" role="listbox">
 
@@ -35,30 +34,30 @@
 
 			</div>
 
-            <?php if ($count > 1): ?>
+			<?php if ( $count > 1 ) : ?>
 
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 
-                    <span class="sr-only">Previous</span>
+					<span class="sr-only">Previous</span>
 
-                </a>
+				</a>
 
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
 
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
 
-                    <span class="sr-only">Next</span>
+					<span class="sr-only">Next</span>
 
-                </a>
+				</a>
 
-            <?php endif; ?>
+			<?php endif; ?>
 
 		</div><!-- .carousel -->
 
 		<script>
-			jQuery( ".carousel-item" ).first().addClass( "active" );
+			jQuery(".carousel-item").first().addClass("active");
 		</script>
 
 	</div>
