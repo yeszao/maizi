9268f0f735fd01d85bac571bdf05b4fb913c3546
yeszao <yeszao@qq.com>
2019-09-27 18:57:15 +0800

    Reduce answer to 4 type: c, c++, java, python

--- loop-templates/content-aside.php
+++ loop-templates/content-aside.php
@@ -24,50 +24,58 @@
         'python' =>'Python',
         'java' => 'Java',
         'c' => 'C',
-        'cplusplus' => 'C++',
-        'csharp' => 'C#',
-        'javascript' => 'JavaScript',
+        'cpp' => 'C++',
     ]
     ?>
 
 	<div class="entry-content">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <?php $content = get_the_content() ?>
 
-            <li class="nav-item">
-                <a class="nav-link active<?php if (!$content): ?> text-muted<?php endif; ?>" id="analyse-tab"
-                   data-toggle="tab" href="#analyse" role="tab" aria-controls="analyse" aria-selected="true">
-                    <span class="dashicons dashicons-code-standards"></span>
-                    <span>答案解析</span>
-                </a>
-            </li>
+        <div class="d-flex justify-content-between flex-wrap">
+            <h2>答案</h2>
+            <div>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <?php $content = get_the_content() ?>
 
-            <?php foreach ($languages as $name => $title) : ?>
-            <li class="nav-item">
-                <a class="nav-link<?php if ( !get_field($name) ): ?> text-muted<?php endif ?>" id="<?php echo $name?>-tab"
-                   data-toggle="tab" href="#<?php echo $name ?>" role="tab" aria-controls="<?php echo $name ?>" aria-selected="false">
-                    <span><?php echo $title ?></span>
-                </a>
-            </li>
-            <?php endforeach; ?>
-        </ul>
+                    <?php foreach ($languages as $name => $title) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link<?php if ( !get_field($name) ): ?> text-muted<?php endif ?>" id="<?php echo $name?>-tab"
+                           data-toggle="tab" href="#<?php echo $name ?>" role="tab" aria-controls="<?php echo $name ?>" aria-selected="false">
+                            <span><?php echo $title ?></span>
+                        </a>
+                    </li>
+                    <?php endforeach; ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-muted" id="empty-tab"
+                           data-toggle="tab" href="#empty" role="tab" aria-controls="empty" aria-selected="false">
+                            <span>不显示</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
         <div class="tab-content my-4" id="myTabContent">
-            <div class="tab-pane fade show active" id="analyse" role="tabpanel" aria-labelledby="analyse-tab">
-                <?php echo $content; ?>
-            </div>
-
             <?php foreach ($languages as $name => $title) : ?>
             <div class="tab-pane fade" id="<?php echo $name ?>" role="tabpanel" aria-labelledby="<?php echo $name ?>-tab">
 
                 <?php if ($field = get_field($name)): ?>
-                    <?php echo $field ?>
+                    <pre><code  class="language language-<?php echo $name ?>"><?php echo $field ?></code></pre>
                 <?php else: ?>
-                    <p class="text-muted"><i>暂无<?php echo $title ?>解法</i></p>
+                    <p class="text-muted"><i>暂无<?php echo $title ?>语言解法</i></p>
                 <?php endif; ?>
 
             </div>
             <?php endforeach; ?>
+
+            <div class="tab-pane fade" id="empty" role="tabpanel" aria-labelledby="empty-tab">
+                    <p class="text-muted"><i>（不显示答案）</i></p>
+            </div>
+        </div>
+
+        <h2>解析</h2>
+        <div class="answer-analysis">
+            <?php echo $content; ?>
         </div>
 
         <p class="text-center">
