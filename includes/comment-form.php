b590cb1d27d92a574fd0ad3422a44b81a0e9a013
yeszao <yeszao@qq.com>
2019-10-09 14:11:33 +0800

    Adjust comment-form.php code

--- includes/comment-form.php
+++ includes/comment-form.php
@@ -1,21 +1,18 @@
 <?php
 /**
- * Comment layout.
+ * Comment form layout.
  *
  * @package maizi
  */
 
-// Comments form.
-add_filter( 'comment_form_default_fields', 'bootstrap3_comment_form_fields' );
 
 /**
  * Creates the comments form.
  *
  * @param string $fields Form fields.
- *
  * @return array
  */
-function bootstrap3_comment_form_fields( $fields ) {
+function bootstrap_comment_form_fields( $fields ) {
 	$commenter = wp_get_current_commenter();
 	$req       = get_option( 'require_name_email' );
 	$aria_req  = ( $req ? " aria-required='true'" : '' );
@@ -34,17 +31,16 @@ function bootstrap3_comment_form_fields( $fields ) {
 
 	return $fields;
 }
+add_filter( 'comment_form_default_fields', 'bootstrap_comment_form_fields' );
 
-add_filter( 'comment_form_defaults', 'bootstrap3_comment_form' );
 
 /**
  * Builds the form.
  *
  * @param string $args Arguments for form's fields.
- *
  * @return mixed
  */
-function bootstrap3_comment_form( $args ) {
+function bootstrap_comment_form( $args ) {
 	$args['comment_field'] = '<div class="form-group comment-form-comment">
     <label for="comment">' . _x( 'Comment', 'noun', 'maizi' ) . ( ' <span class="required">*</span>' ) . '</label>
     <textarea class="form-control" id="comment" name="comment" aria-required="true" cols="45" rows="8"></textarea>
@@ -52,3 +48,4 @@ function bootstrap3_comment_form( $args ) {
 	$args['class_submit']  = 'btn btn-secondary'; // since WP 4.1.
 	return $args;
 }
+add_filter( 'comment_form_defaults', 'bootstrap_comment_form' );
