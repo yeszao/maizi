a740dbdb8f8af2f64a295b93a87c5c70aa5870a7
yeszao <yeszao@qq.com>
2019-10-09 15:51:06 +0800

    Check previous and next post link before display

--- includes/previous-next.php
+++ includes/previous-next.php
@@ -6,14 +6,19 @@
 function get_previous_and_next_post() {
     ?>
     <div class="d-flex justify-content-between flex-wrap my-4 font-weight-bold">
-        <div class="">
-            <?php _e('Previous post:', 'maizi') ?>
-            <?php next_post_link('%link') ?>
+
+        <div>
+            <?php if ($link = get_next_post_link('%link')) : ?>
+                <?php _e('Previous post:', 'maizi') ?> <?php echo $link; ?>
+            <?php endif; ?>
         </div>
-        <div class="">
-            <?php _e('Next post:', 'maizi') ?>
-            <?php previous_post_link('%link') ?>
+
+        <div>
+            <?php if ($link = get_previous_post_link('%link')) : ?>
+                <?php _e('Next post:', 'maizi') ?> <?php echo $link; ?>
+            <?php endif; ?>
         </div>
+
     </div>
 
 <?php
